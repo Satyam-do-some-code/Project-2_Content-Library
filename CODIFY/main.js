9d275eb4243a48066ec78ccaf8e86d4178bcd588
Satyaprakash Sharma <sharmasatyaprakash169@gmail.com>
2024-09-17 01:02:06 +0530

    Course section is in underwork

--- CODIFY/main.js
+++ CODIFY/main.js
@@ -1,46 +1,46 @@
-// //change navbar style on scroll
+//change navbar style on scroll
 
-// window.addEventListener('scroll', () =>{
-//     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-// })
-
-
-// //  Hide/Show the faq answer
-
-// const faqs = document.querySelectorAll('.faq');
-
-// faqs.forEach(faq =>{
-//     faq.addEventListener('click', () => {
-//         faq.classList.toggle('open');
-
-//         // changing the + icon to - onclick 
-//         const icon = faq.querySelector('.faq__icon i');
-//         if(icon.className === 'fa-solid fa-plus'){
-//             icon.className = 'fa-solid fa-minus';
-//         }else{
-//             icon.className = 'fa-solid fa-plus';
-//         }
-//     })
-// })
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
 
 
-// // Hide/Show the nav menu 
-// const menu = document.querySelector(".nav__menu");
-// const menuBtn = document.querySelector("#open-menu-btn");
-// const closeBtn = document.querySelector("#close-menu-btn");
+//  Hide/Show the faq answer
 
-// menuBtn.addEventListener('click', () =>{
-//     menu.style.display = "flex";
-//     closeBtn.style.display = "inline-block";
-//     menuBtn.style.display = "none";
-// })
+const faqs = document.querySelectorAll('.faq');
 
-// // Close nav menu
+faqs.forEach(faq =>{
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
 
-// const closenav = () => {
-//     menu.style.display = "none";
-//     closeBtn.style.display = "none";
-//     menuBtn.style.display = "inline-block";
-// }
+        // changing the + icon to - onclick 
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
+        }else{
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
 
-// closeBtn.addEventListener('click' , closenav)
+
+// Hide/Show the nav menu 
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () =>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// Close nav menu
+
+const closenav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click' , closenav)
